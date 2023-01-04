c52ea6bdd78ed7df106cc954fea367406c05780f
dldnjswns31 <dldnjswns32@gmail.com>
2023-01-04 19:27:52 +0900

    feat: Route 구현 - /auth: 로그인 및 회원가입 - /todo: todo CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Auth from "./routes/Auth";
+import Todo from "./routes/Todo";
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Routes>
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/todo" element={<Todo />} />
+    </Routes>
+  );
 }
 
 export default App;
